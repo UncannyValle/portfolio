ebd9c196d73852569a366a16ffb99af72e6dc480
Julian V <jvalle0926@gmail.com>
2020-01-12 14:43:39 -0800

    messed around with state in header

--- src/components/header.js
+++ src/components/header.js
@@ -1,25 +1,50 @@
 import { Link } from "gatsby"
-import React from "react"
+import React, { useState, useEffect } from "react"
 import styles from "./header.module.css"
-const Header = () => (
-  <header className={styles.header}>
-    <nav>
-      <ul className={styles.navbar}>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about/">About</Link>
-        </li>
-        <li>
-          <Link to="/projects/">Projects</Link>
-        </li>
-        <li>
-          <Link to="/contact/">Contact</Link>
-        </li>
-      </ul>
-    </nav>
-  </header>
-)
+const Header = () => {
+  const [currentTab, setCurrentTab] = useState("home")
 
+  const tabStyles = tab => (currentTab === tab ? styles.currentTab : null)
+
+  return (
+    <header className={styles.header}>
+      <nav>
+        <ul className={styles.navbar}>
+          <li>
+            <Link className={tabStyles("home")} to="/">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link
+              className={tabStyles("about")}
+              onClick={setCurrentTab("about")}
+              to="/about/"
+            >
+              About
+            </Link>
+          </li>
+          <li>
+            <Link
+              className={tabStyles("projects")}
+              onClick={setCurrentTab("projects")}
+              to="/projects/"
+            >
+              Projects
+            </Link>
+          </li>
+          <li>
+            <Link
+              className={tabStyles("contact")}
+              onClick={setCurrentTab("contact")}
+              to="/contact/"
+            >
+              Contact
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
 export default Header
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import styles from "./projects.module.css"
 
 const Projects = () => (
-    <Layout>
-        <SEO title= "Projects Page" />
-    <h1>Projects Page</h1>
-    </Layout>
+  <Layout>
+    <SEO title="Projects Page" />
+    <div className={styles.projects}>
+      <h1>Projects Page</h1>
+    </div>
+  </Layout>
 )
 export default Projects
