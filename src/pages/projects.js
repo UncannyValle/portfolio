1ddb4dbb08bf9ac2fb2af92ccc1c8573d06f5b4c
uncannyvalle <jvalle0926@gmail.com>
2020-02-02 21:35:19 -0800

    added new projects

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -7,6 +7,9 @@ import Skills from "../components/skills"
 import atolePic from "../images/atole_pic.png"
 import videothequePic from "../images/videothequePic.png"
 import dannyPic from "../images/dannyAdventurePic.png"
+import moviePic from "../images/movie.png"
+import markdownPic from "../images/markdownPic.png"
+
 const Projects = () => (
   <Layout>
     <SEO title="Projects Page" />
@@ -27,20 +30,33 @@ const Projects = () => (
           tech={"Gatsby | React | JS | CSS | HTML | NetlifyCMS"}
           gitHub={"https://github.com/UncannyValle/Lena-s-Videotheque"}
         />
-        <Skills 
+        <Skills
           image={dannyPic}
           site={"https://lbalvarez8.github.io/Happy-Birthday-Danny/"}
-          text={"A JS browser game, worked in a team and helped code the gameplay"}
+          text={
+            "A JS browser game, worked in a team and helped code the gameplay"
+          }
           tech={"Javascript | jQuery | CSS | HTML | Git "}
           gitHub={"https://github.com/UncannyValle/Danny-Boy"}
         />
-        {/* <Skills 
-
-
-        text={"Project for freeCodeCamp using React and then ported to gatsby to host in Netlify"}
-        tech={'React | JS | CSS | HTML | Gatsby | Netlify'}
-        gitHub={"https://github.com/UncannyValle/MovieQuote-machine"}
-        /> */}
+        <Skills
+          image={moviePic}
+          site={"https://movie-quote-machine.netlify.com/"}
+          text={
+            "Project for freeCodeCamp using React and then ported to Gatsby to host in Netlify"
+          }
+          tech={"React | JS | CSS | HTML | Netlify"}
+          gitHub={"https://github.com/UncannyValle/MovieQuote-machine"}
+        />
+        <Skills
+          image={markdownPic}
+          site={"https://mrkdw-preview-machine.netlify.com/"}
+          text={
+            "A markdown previewer using React"
+          }
+          tech={"React | JS | CSS | HTML | Netlify"}
+          gitHub={"https://github.com/UncannyValle/markdown-previewer"}
+        />
       </div>
     </div>
   </Layout>
