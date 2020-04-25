6633b999c7a3c3048c3408509bcf7028857c6db3
Julian Valle <julianv@atolemedia.com>
2020-04-24 18:47:17 -0600

    ok lets check this out

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,10 +10,11 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
+        name: `blog`,
+        path: `${__dirname}/blog`,
       },
     },
+    `gatsby-transformer-remark`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
