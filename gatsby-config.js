8f3163dd98f361903dc8aedea353e7ac1e67cd71
UncannyValle <julianv@atolemedia.com>
2020-06-02 15:25:56 -0700

    bug fixing styled components

--- gatsby-config.js
+++ gatsby-config.js
@@ -44,7 +44,12 @@ module.exports = {
         ],
       },
     },
-    `gatsby-plugin-styled-components`,
+    {
+      resolve: `gatsby-plugin-styled-components`,
+      options: {
+        // Add any options here
+      },
+    },,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
