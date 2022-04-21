7cda855f41afbc64e3dd358d5c76f83a4710e744
Julian Valle <julian.valle@enovational.com>
2022-04-20 20:17:53 -0600

    a test

--- src/components/projects.js
+++ src/components/projects.js
@@ -128,33 +128,7 @@ export const Projects = (props) => {
       tech: "React | JS | CSS | HTML | Netlify",
       gitHub: "https://github.com/UncannyValle/calcutron",
     },
-    // {
-    //   title: "Avocado Clock",
-    //   src: getImage(data.avocado),
-    //   alt: "avocado clock",
-    //   site: "https://avocadoclock.netlify.app/",
-    //   text: "A work timer, to keep you motivated",
-    //   tech: " React | JS| React-Spring",
-    //   gitHub: "https://github.com/UncannyValle/Avocado-Clock",
-    // },
-    // {
-    //   title: "Movie Quoter",
-    //   src: getImage(data.movie),
-    //   alt: "movie quote machine",
-    //   site: "https://movie-quote-machine.netlify.com/",
-    //   text: "Built in React, uses an external API to access data",
-    //   tech: "React | JS | CSS | HTML | Netlify",
-    //   gitHub: "https://github.com/UncannyValle/MovieQuote-machine",
-    // },
-    // {
-    //   title: "Mario Drum Machine",
-    //   src: getImage(data.mario),
-    //   alt: "mario drummer",
-    //   site: "https://mp-soundmaker.netlify.com",
-    //   text: "A sound tester with the music of Mario Paint",
-    //   tech: "React | JS | CSS | HTML | Netlify",
-    //   gitHub: "https://github.com/UncannyValle/MarioPaintSoundMachine",
-    // },
+    
   ]
   const boxTrail = useTrail(skillList.length, {
     opacity: 1,
